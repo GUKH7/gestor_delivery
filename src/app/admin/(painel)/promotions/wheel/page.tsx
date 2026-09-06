97ed262605cdf05bc2ef41875044888ad900cfdb
GUKH7 <keiohirakawa@gmail.com>
2026-09-06 08:48:55 -0300

    feat: exibir dashboard da roleta

--- src/app/admin/(painel)/promotions/wheel/page.tsx
+++ src/app/admin/(painel)/promotions/wheel/page.tsx
@@ -1,9 +1,11 @@
 import RewardCheckoutRules from "./RewardCheckoutRules";
+import WheelCampaignMetrics from "./WheelCampaignMetrics";
 import WheelCampaignWorkspace from "./WheelCampaignWorkspace";
 
 export default function PromotionsWheelPage() {
   return (
     <>
+      <WheelCampaignMetrics />
       <WheelCampaignWorkspace />
       <RewardCheckoutRules />
     </>
